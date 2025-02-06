b283cc7e0e3c076929ab113e792789920017e2d6
Konstantin Lyashenko <lyashenkokv@gmail.com>
2025-02-06 13:25:28 +0300

    Initial Commit

---
+++ AutodocNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutodocNews
+//
+//  Created by Konstantin Lyashenko on 06.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
