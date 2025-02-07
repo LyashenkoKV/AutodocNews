cdd34757b1b7657574064b5042ac9cf6680b061f
Konstantin Lyashenko <lyashenkokv@gmail.com>
2025-02-07 23:31:58 +0300

    feat (AN-00): Add helpers

---
+++ AutodocNews/Helpers/NewsCellFactory.swift
@@ -0,0 +1,41 @@
+//
+//  NewsCellFactory.swift
+//  AutodocNews
+//
+//  Created by Konstantin Lyashenko on 07.02.2025.
+//
+
+import UIKit
+
+@MainActor
+enum NewsCellFactory {
+    static func configureCell(
+        for indexPath: IndexPath,
+        tableView: UITableView,
+        viewModel: NewsDetailsViewModel,
+        completion: @escaping () -> Void?
+    ) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell: NewsHStackCell = tableView.dequeueReusableCell()
+            cell.configure(headline: viewModel.news.title) {
+                completion()
+            }
+            return cell
+        case 1:
+            let cell: NewsDateCell = tableView.dequeueReusableCell()
+            cell.configure(date: viewModel.news.publishedDate.formattedRelativeDate())
+            return cell
+        case 2:
+            let cell: NewsTextCell = tableView.dequeueReusableCell()
+            cell.configure(text: viewModel.news.description)
+            return cell
+        case 3:
+            let cell: NewsImagesCell = tableView.dequeueReusableCell()
+            cell.configure(with: viewModel.images)
+            return cell
+        default:
+            fatalError("Unexpected cell index")
+        }
+    }
+}
--- AutodocNews/Views/NewsDetails/NewsDetailViewController.swift
+++ AutodocNews/Views/NewsDetails/NewsDetailViewController.swift
@@ -15,8 +15,6 @@ final class NewsDetailViewController: UIViewController {
 
     private let viewModel: NewsDetailsViewModel
     private var cancellables = Set<AnyCancellable>()
-    private let formatter = DateFormatter()
-    private let calendar = Calendar.current
 
     private lazy var tableView: UITableView = {
         let tv = UITableView(frame: .zero, style: .insetGrouped)
@@ -50,7 +48,7 @@ final class NewsDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        setupTableView()
+        setupUI()
         bindViewModel()
 
         let targetSize = CGSize(width: view.bounds.width, height: 300)
@@ -61,7 +59,21 @@ final class NewsDetailViewController: UIViewController {
 
     // MARK: - Setup UI
 
-    private func setupTableView() {
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        configureTableView()
+    }
+
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(NewsHStackCell.self)
+        tableView.register(NewsDateCell.self)
+        tableView.register(NewsTextCell.self)
+        tableView.register(NewsImagesCell.self)
         view.setupView(tableView)
         tableView.constraintEdges(to: view)
     }
@@ -75,37 +87,13 @@ final class NewsDetailViewController: UIViewController {
             .store(in: &cancellables)
     }
 
-    private func showSortActionSheet() {
-        let alertController = UIAlertController(
+    private func showShareActions() {
+        ActionSheetPresenter.presentShareActions(
+            from: self,
             title: viewModel.news.title,
             message: viewModel.news.publishedDate.formattedRelativeDate(),
-            preferredStyle: .actionSheet
+            url: viewModel.news.fullUrl
         )
-
-        let copyAction = UIAlertAction(title: "Скопировать ссылку", style: .default) { [weak self] _ in
-            guard let url = self?.viewModel.news.fullUrl else { return }
-            UIPasteboard.general.string = url
-        }
-
-        let image = UIImage(systemName: "doc.on.doc")
-        image?.withTintColor(.white, renderingMode: .alwaysOriginal)
-
-        copyAction.setValue(image, forKey: "image")
-
-        let shareAction = UIAlertAction(title: "Поделиться", style: .default) { [weak self] _ in
-            guard let url = self?.viewModel.news.fullUrl else { return }
-            let activityVC = UIActivityViewController(activityItems: [url], applicationActivities: nil)
-            self?.present(activityVC, animated: true)
-        }
-        shareAction.setValue(UIImage(systemName: "square.and.arrow.up"), forKey: "image")
-
-        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
-
-        alertController.addAction(copyAction)
-        alertController.addAction(shareAction)
-        alertController.addAction(cancelAction)
-
-        present(alertController, animated: true, completion: nil)
     }
 }
 
@@ -125,28 +113,12 @@ extension NewsDetailViewController: UITableViewDelegate, UITableViewDataSource {
         cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
 
-        switch indexPath.row {
-        case 0:
-            let cell: NewsHStackCell = tableView.dequeueReusableCell()
-            cell.configure(headline: viewModel.news.title) { [weak self] in
-                self?.showSortActionSheet()
+        NewsCellFactory.configureCell(
+            for: indexPath,
+            tableView: tableView,
+            viewModel: viewModel) { [weak self] in
+                self?.showShareActions()
             }
-            return cell
-        case 1:
-            let cell: NewsDateCell = tableView.dequeueReusableCell()
-            cell.configure(date: viewModel.news.publishedDate.formattedRelativeDate())
-            return cell
-        case 2:
-            let cell: NewsTextCell = tableView.dequeueReusableCell()
-            cell.configure(text: viewModel.news.description)
-            return cell
-        case 3:
-            let cell: NewsImagesCell = tableView.dequeueReusableCell()
-            cell.configure(with: viewModel.images)
-            return cell
-        default:
-            fatalError("Unexpected row")
-        }
     }
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
