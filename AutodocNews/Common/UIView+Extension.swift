71a31db39dba494fe4e0f126c9c93e7605542919
Konstantin Lyashenko <lyashenkokv@gmail.com>
2025-02-18 13:38:54 +0300

    fix (main): Minor fix optimisation

--- AutodocNews/Common/UIView+Extension.swift
+++ AutodocNews/Common/UIView+Extension.swift
@@ -11,6 +11,7 @@ extension UIView {
     func setupView(_ subview: UIView) {
         subview.translatesAutoresizingMaskIntoConstraints = false
         //subview.layer.borderWidth = 1
+        isOpaque = true
         addSubview(subview)
     }
 }
